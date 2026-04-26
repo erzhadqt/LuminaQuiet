6511d823883d92f6186f49f7ca330aaa0d7ee715
Ahlan-nour <sencioahlannour@gmail.com>
2026-04-27 03:03:46 +0800

    bug fixes and used db levels(4)

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+REDIS_URL = config("REDIS_URL")
 
 from decouple import config, Csv
 import dj_database_url
